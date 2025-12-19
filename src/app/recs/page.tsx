bf839aae2b500ea282055e0877e0a682a463689c
manue7mokua <manue7codes@gmail.com>
2025-12-19 02:56:48 -0500

    Add back to home navigation link to recs page

--- src/app/recs/page.tsx
+++ src/app/recs/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { Bookshelf } from "@/components/bookshelf";
 import { AppHeader } from "@/components/AppHeader";
 import { useTheme } from "next-themes";
@@ -13,6 +14,15 @@ export default function RecsPage() {
       <AppHeader barClassName="h-14 items-end pb-2" />
 
       <div className="h-full pt-14 flex flex-col">
+        {/* Back to home link */}
+        <div className="absolute top-18 left-6 z-10">
+          <Link 
+            href="/" 
+            className="font-mono text-sm text-foreground underline hover:opacity-70 transition-opacity"
+          >
+            ← Back to home
+          </Link>
+        </div>
         <div className="shrink-0 pt-12 pb-0 text-center">
           <h1 
             className="font-cinzel text-xs tracking-[0.3em] uppercase translate-y-1"
